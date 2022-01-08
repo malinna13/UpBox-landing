bae63ede093b80f3c1a51b6da1b7e4937fac1396
Inna Malinovskaya <malinna13@gmail.com>
2022-01-08 10:26:09 +0600

    feat: mobile menu

---
+++ public/script.js
@@ -0,0 +1,8 @@
+//Mobile menu
+
+const btnNavEl = document.querySelector(".btn-mobile-nav");
+const headerEl = document.querySelector(".header");
+
+btnNavEl.addEventListener("click", function () {
+  headerEl.classList.toggle("nav-open");
+});
